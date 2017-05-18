c364f4ea46066cd850e1699ea16d72bdc48f4087
Gita Guntaka <gitasri.reddy@gmail.com>
2017-05-17 23:19:40 -0700

    initial commit

--- shopping.js
+++ shopping.js
@@ -1,16 +1,28 @@
 (function(){
- var app= angular.module("app",[]);
+ var app =  angular.module("app",[]);
  app.controller("myController",function($scope){
    
    
-   $scope.products=["Milk","coffee","tea","abc"];
+   $scope.products = ["Milk","coffee","tea","abc"];
    
-   $scope.addItem= function(){
-     $scope.products.push($scope.add);
-     $scope.add="";
+   $scope.addItem = function(){
+
+   	$scope.errorTxt =  '';
+
+
+   	if($scope.products.indexOf($scope.add ) == -1 ){
+
+   		$scope.products.push($scope.add);
+     	$scope.add =  " ";
+     
+
+   	} else{
+
+   		$scope.errorTxt = " This item is already exits  in your list"
+   	}
      
    };
-   $scope.remove=function(index){
+   $scope.remove =  function(index){
      $scope.products.splice(index,1);
    }
    
